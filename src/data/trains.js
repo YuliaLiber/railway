184fe4f82a724a258f11b62490049af1ad47bd7a
YuliaLiber <liberyula07@gmail.com>
2026-04-27 20:56:36 -0700

    add more train data

--- src/data/trains.js
+++ src/data/trains.js
@@ -22,5 +22,29 @@ export const trains = [
     to: "Kyiv",
     time: "12:00",
     duration: "4h"
-  }
+  },
+  {
+  id: 4,
+  number: "IC-404",
+  from: "Lviv",
+  to: "Kyiv",
+  time: "14:20",
+  duration: "5h 30m"
+},
+{
+  id: 5,
+  number: "IC-505",
+  from: "Odesa",
+  to: "Dnipro",
+  time: "16:10",
+  duration: "6h"
+},
+{
+  id: 6,
+  number: "IC-606",
+  from: "Kyiv",
+  to: "Kharkiv",
+  time: "18:45",
+  duration: "7h 15m"
+}
 ];
