846f7728064ca15727cc5cfb6e4fc30a5abe395f
YuliaLiber <liberyula07@gmail.com>
2026-05-09 21:13:30 -0700

    connect wagon selector with booking page

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,14 +1,23 @@
 import { useParams } from "react-router-dom";
+import { useState } from "react";
+import WagonSelector from "../components/WagonSelector";
 
 export default function Booking() {
 
   const { trainId } = useParams();
+  const [wagon, setWagon] = useState(1);
 
   return (
     <div>
       <h1>🎟 Booking Page</h1>
 
       <p>Train ID: {trainId}</p>
+
+      <WagonSelector onSelect={setWagon} />
+
+      <h3 style={{ marginTop: "20px" }}>
+        Selected wagon: {wagon}
+      </h3>
     </div>
   );
 }
