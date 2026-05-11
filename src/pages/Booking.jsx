9d766a3faaa3eceb731f694b27dbbf3c76732c5b
YuliaLiber <liberyula07@gmail.com>
2026-05-11 12:25:13 -0700

    load bookings from localStorage

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,35 +1,61 @@
 import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 import WagonSelector from "../components/WagonSelector";
+
 import { saveBooking } from "../services/bookingService";
+import { getBookings } from "../services/bookingService";
 
 export default function Booking() {
 
   const { trainId } = useParams();
+
   const [wagon, setWagon] = useState(1);
 
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    const data = getBookings();
+    setBookings(data);
+  }, []);
+
   function handleBook() {
-  saveBooking({
-    trainId,
-    wagon,
-    seat: 1
-  });
-}
+
+    saveBooking({
+      trainId,
+      wagon,
+      seat: 1
+    });
+
+    const updated = getBookings();
+    setBookings(updated);
+  }
 
   return (
-  <div>
-    <h1>🎟 Booking Page</h1>
+    <div>
 
-    <p>Train ID: {trainId}</p>
+      <h1>🎟 Booking Page</h1>
 
-    <WagonSelector onSelect={setWagon} />
+      <p>Train ID: {trainId}</p>
 
-    <h3>Selected wagon: {wagon}</h3>
+      <WagonSelector onSelect={setWagon} />
 
-    <button onClick={handleBook}>
-      🎟 Book ticket
-    </button>
+      <h3>Selected wagon: {wagon}</h3>
 
-  </div>
-);
+      <button onClick={handleBook}>
+        🎟 Book ticket
+      </button>
+
+      <h3>Saved bookings:</h3>
+
+      {bookings.map((b, index) => (
+        <p key={index}>
+          Train: {b.trainId} |
+          Wagon: {b.wagon} |
+          Seat: {b.seat}
+        </p>
+      ))}
+
+    </div>
+  );
 }
