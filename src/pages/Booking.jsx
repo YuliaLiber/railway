cd517115e21116828385603bf27467af8dc8f28f
YuliaLiber <liberyula07@gmail.com>
2026-05-11 10:17:56 -0700

    save bookings to localStorage

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,23 +1,35 @@
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import WagonSelector from "../components/WagonSelector";
+import { saveBooking } from "../services/bookingService";
 
 export default function Booking() {
 
   const { trainId } = useParams();
   const [wagon, setWagon] = useState(1);
 
+  function handleBook() {
+  saveBooking({
+    trainId,
+    wagon,
+    seat: selectedSeat
+  });
+}
+
   return (
-    <div>
-      <h1>🎟 Booking Page</h1>
+  <div>
+    <h1>🎟 Booking Page</h1>
 
-      <p>Train ID: {trainId}</p>
+    <p>Train ID: {trainId}</p>
 
-      <WagonSelector onSelect={setWagon} />
+    <WagonSelector onSelect={setWagon} />
 
-      <h3 style={{ marginTop: "20px" }}>
-        Selected wagon: {wagon}
-      </h3>
-    </div>
-  );
+    <h3>Selected wagon: {wagon}</h3>
+
+    <button onClick={handleBook}>
+      🎟 Book ticket
+    </button>
+
+  </div>
+);
 }
