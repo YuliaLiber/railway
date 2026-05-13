0f3a1f334d8c8567a1d2ef2fa82e00a9118d282a
YuliaLiber <liberyula07@gmail.com>
2026-05-12 18:38:16 -0700

    improve responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Booking from "./pages/Booking";
+import "./App.css";
 
 function App() {
   return (
